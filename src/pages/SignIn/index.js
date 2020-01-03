8a53586486be336ae5811b4320f4a20efc318ff1
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-01-03 15:52:50 -0300

    [Add] componentes input e botao

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,11 +4,21 @@ import {View, Text} from 'react-native';
 // import { Container } from './styles';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>Oloko meu</Text>
+
+      <Input
+        style={{marginTop: 30}}
+        icon="call"
+        placeholder="Digite o seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }

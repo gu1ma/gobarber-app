ec5bdb9492b362c4a605f9764aee9225b116e34e
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-01-03 17:00:01 -0300

    [Add] configurando statusbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {StatusBar} from 'react-native';
 import '~/config/ReactotronConfig';
 
 import {Provider} from 'react-redux';
@@ -9,6 +9,7 @@ import Routes from '~/routes';
 
 const App = () => (
   <Provider store={store}>
+    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
     <Routes />
   </Provider>
 );

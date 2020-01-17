85e24a1d94c30e899d0a0711a770eceddbc2a924
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-01-17 19:46:26 -0300

    [Add] estilizando rotas

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default (SignedIn = false) =>
   createAppContainer(
@@ -14,9 +15,22 @@ export default (SignedIn = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              keyboardHidesTabBar: true,
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          },
+        ),
       },
       {
         initialRouteName: SignedIn ? 'App' : 'Sign',

c5bdd72b4c7b5b07e8f40efede60208985c538af
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-01-25 19:58:49 -0300

    [Add] Datetime por plataforma

---
+++ src/components/DateInput/index.android.js
@@ -0,0 +1,34 @@
+import React, {useMemo} from 'react';
+import {DatePickerAndroid} from 'react-native';
+import {format} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, DateButton, DateText} from './styles';
+
+export default function DateInput({date, onChange}) {
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", {locale: pt}),
+    [date],
+  );
+
+  async function handleOpenPicker() {
+    const {action, year, mounth, day} = await DatePickerAndroid.open({
+      mode: 'spinner',
+      date,
+    });
+
+    if (action === DatePickerAndroid.dateSetAction) {
+    }
+  }
+
+  return (
+    <Container>
+      <DateButton onPress={handleOpenPicker}>
+        <Icon name="event" color="#FFF" size={20} />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+    </Container>
+  );
+}
